e8e23817088d641405d02afb2c8d1fff4e2a1906
Aly Mohamed <aly.mohamed@ahram.org.eg>
2024-10-16 21:12:22 +0300

    Add project files.

---
+++ DoctorERP_v3.00/DataSources.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DoctorERP_v3_00
+{
+    public static class DataSources
+    {
+        public static PatientsDataSet PatientsDataSet
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ DoctorERP_v3.00/NullableSpinEditor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.Reflection;
+using System.Windows.Forms;
+
+using Telerik.WinControls;
+using Telerik.WinControls.UI;
+
+namespace DoctorERP_v3_00
+{
+    public class NullableSpinEditor : RadSpinEditor
+    {
+        public NullableSpinEditor()
+        {
+            this.EnableNullValueInput = true;
+        }
+    }  
+}
